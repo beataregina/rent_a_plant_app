b27a74c8ebd3c42f078985899becf6e1753ee16c
Beata VÃoelker <beataregina.voelker@gmail.com>
2022-05-24 12:58:34 +0200

    validationssssssss

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,15 +3,15 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :end_date_is_after_start_date
+  # validates :end_date_is_after_start_date
 
-  private
+  # private
 
-  def end_date_is_after_start_date
-    return if end_date.blank? || start_date.blank?
+  # def end_date_is_after_start_date
+  #   return if end_date.blank? || start_date.blank?
 
-    if end_date <= start_date
-      errors.add(:end_date, "cannot be before the start date")
-    end
-  end
+  #   if end_date <= start_date
+  #     errors.add(:end_date, "cannot be before the start date")
+  #   end
+  # end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :user
-  # validates :name, :plant_type, :age, :city, :price, presence: true
-  # validates :age, numericality: { only_integer: true }
+
+  validates :name, :plant_type, :age, :city, :price, presence: true
+  validates :age, numericality: { only_integer: true }
+
 end
