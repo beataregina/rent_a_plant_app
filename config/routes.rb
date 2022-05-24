4960564cbd04dae4fa02967e9eda3a849af64f3c
isabel bruda <ibrudap@gmail.com>
2022-05-24 14:05:55 +0200

    plants controller updated with all methods, plants/view & plants/show created. _form created. No styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
 
-  resources :users, only: %i[new create login] do
-    resources :plants, only: %i[index show new create edit update]
-  end
-
-  resources :plants, only: [:destroy]
+  resources :plants
 end
