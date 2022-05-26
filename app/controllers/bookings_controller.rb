1333f0f2d596ce1f38b5023184f19bf727a2870b
Beata VÃoelker <beataregina.voelker@gmail.com>
2022-05-26 18:22:09 +0200

    methods inserted in the controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,12 @@
 class BookingsController < ApplicationController
-  before_action :find_booking, only: %i[destroy show]
-  before_action :find_plant, only: %i[new create index show]
+  before_action :find_booking, only: %i[destroy show update edit]
+  before_action :find_plant, only: %i[new create index show update edit]
 
   def new
     @booking = Booking.new
   end
 
   def index
-
   end
 
   def show
@@ -19,6 +18,14 @@ class BookingsController < ApplicationController
     @booking.plant = @plant
     @booking.save!
     # redirect_to plant_booking_path(@plant, @booking)
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
   end
 
   def destroy
