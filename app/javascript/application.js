6ad457b809fcebe47dab8b49c7751182c49a0cc0
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2022-05-27 13:20:21 +0200

    some stuff from the booking fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,13 +4,3 @@ import "./controllers"
 import "bootstrap"
 
 
-const btn = document.querySelector(".plant__btn");
-
-btn.addEventListener("click", (event) => {
-  event.preventDefault;
-  const baseUrl = window.location.origin;
-  console.log('redirecting to dashboard')
-  setTimeout(function(){
-    document.location.href= `${baseUrl}/dashboard`;
- }, 4000);
-});
